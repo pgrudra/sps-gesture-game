abc098d57bc30459b71dd0e684f7168261c17360
pgrudra <pgrudrakshi@gmail.com>
2025-06-07 12:17:25 +0530

    improvs

--- src/js/game/GameEngine.js
+++ src/js/game/GameEngine.js
@@ -425,7 +425,7 @@ export class GameEngine {
         }
         
         // More gradual speed scaling
-        this.speed = Math.min(1.0 + (this.score * 0.05), 4.0);
+        this.speed = Math.min(1.0 + (this.score * 0.03), 4.0);
         const currentSpeed = this.baseSpeed * this.speed;
         
         // Update obstacles
